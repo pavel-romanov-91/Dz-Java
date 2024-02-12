f6a72a0c0bf16198095d3660cbbdd73e73b513e8
pavel-romanov-91 <101830399+pavel-romanov-91@users.noreply.github.com>
2024-02-12 19:59:31 +0300

    Add files via upload

---
+++ dz/src/dz/dz.java
@@ -0,0 +1,51 @@
+package dz;
+
+import java.util.Scanner;
+
+public class dz {
+	public double dz(double obtained, double total) {
+        return obtained * 100 / total;
+	}
+	public static void main(String[] args) {
+        
+//        System.out.println("“Your time is limited,\r\n"
+//        		+ "so don’t waste it \r\n"
+//        		+ "living someone else’s life”\r\n"
+//        		+ "Steve Jobs");
+//        System.out.println("Bye world...");
+    
+		
+		
+//		dz pc = new dz();
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Введите первое число:");
+//        double obtained = in.nextDouble();
+//        System.out.println("Введите второе число:");
+//        double total = in.nextDouble();
+//        System.out.println(
+//          "Полученый процент: " + pc.dz(obtained, total));
+	
+		
+		
+//		Scanner scan = new Scanner(System.in);
+//	    int numA = scan.nextInt();
+//	    int numB = scan.nextInt();
+//	    int numC = scan.nextInt();
+//	    System.out.println(numA + "" + numB + "" + numC);
+//	
+	
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}

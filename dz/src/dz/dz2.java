87c9315cd21793faa654c0476c724d5dac0741e3
pavel-romanov-91 <101830399+pavel-romanov-91@users.noreply.github.com>
2024-02-17 19:40:48 +0300

    Add files via upload

---
+++ dz/src/dz/dz2.java
@@ -0,0 +1,37 @@
+package dz;
+
+public class dz2 {
+	
+//	    public void sayHello(String firstGuest) {
+//	        System.out.println("Добрый вечер, " + firstGuest + "!");
+//	    }
+//
+//	    public void sayHello(String firstGuest, String secondGuest) {
+//	        System.out.println("Добрый день, " + firstGuest + " и " + secondGuest + "!");
+//	    }
+//	    public static void main(String[] args) {
+//	        dz2 assistant = new dz2();
+//	        assistant.sayHello("Михаил", "Виталий");
+//	    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
